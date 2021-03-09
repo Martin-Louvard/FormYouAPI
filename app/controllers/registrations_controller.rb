9520948f71e158f2bff7830f964f465d314489e6
Martin-Louvard <martin.louvard@gmail.com>
2021-03-09 13:30:00 +0100

    fixed auth bug

--- app/controllers/registrations_controller.rb
+++ app/controllers/registrations_controller.rb
@@ -1,10 +1,23 @@
 class RegistrationsController < Devise::RegistrationsController
-    respond_to :json
   
-    def create
-      build_resource(sign_up_params)
+    def render_resource(resource)
+      if resource.errors.empty?
+        render json: resource
+      else
+        validation_error(resource)
+      end
+    end
   
-      resource.save
-      render_resource(resource)
+    def validation_error(resource)
+      render json: {
+        errors: [
+          {
+            status: '400',
+            title: 'Bad Request',
+            detail: resource.errors,
+            code: '100'
+          }
+        ]
+      }, status: :bad_request
     end
   end
--- config/routes.rb
+++ config/routes.rb
@@ -1,16 +1,12 @@
 Rails.application.routes.draw do
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
-  namespace :api, defaults: { format: :json } do
-    resources :users, only: %w[show]
-  end
-
+  resources :users
   devise_for :users,
-    defaults: { format: :json },
     path: '',
     path_names: {
-      sign_in: 'api/login',
-      sign_out: 'api/logout',
-      registration: 'api/signup'
+      sign_in: 'login',
+      sign_out: 'logout',
+      registration: 'signup'
     },
     controllers: {
       sessions: 'sessions',
